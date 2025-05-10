760ca110b61fadea005a8c5df985fc6711f5ae3c
salahbm <salahbm.001@gmail.com>
2025-05-10 21:45:51 +0900

    feat: translations

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,28 +3,30 @@ import { cn } from '@/lib/utils';
 import { useScrollStore } from '@/stores/useScrollStore';
 import { TabList, Tabs, TabSlot, TabTrigger } from 'expo-router/ui';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function TabLayout() {
   const isScrollingDown = useScrollStore((s) => s.isScrollingDown);
+  const { t } = useTranslation();
   return (
     <Tabs>
       <TabSlot />
       <TabList
         className={cn(
-          'absolute bottom-9 self-center flex-row justify-evenly blur-3xl backdrop-blur-3xl rounded-full p-0.5 transition-all duration-500 drop-shadow-2xl',
+          'absolute bottom-9 self-center flex-row justify-evenly blur-3xl backdrop-blur-3xl rounded-full transition-all duration-500 drop-shadow-2xl',
           isScrollingDown
             ? 'bg-foreground/5 opacity-30'
             : 'bg-foreground/10 opacity-100',
         )}
       >
         <TabTrigger name="Home" href="/" asChild>
-          <TabButton icon="Home">Home</TabButton>
+          <TabButton icon="Home">{t('common.tabs.home')}</TabButton>
         </TabTrigger>
         <TabTrigger name="second" href="/second" asChild>
-          <TabButton icon="Calendar">Calendar</TabButton>
+          <TabButton icon="Calendar">{t('common.tabs.calendar')}</TabButton>
         </TabTrigger>
         <TabTrigger name="third" href="/third" asChild>
-          <TabButton icon="Settings">Settings</TabButton>
+          <TabButton icon="Settings">{t('common.tabs.settings')}</TabButton>
         </TabTrigger>
       </TabList>
     </Tabs>
